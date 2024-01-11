471c885a0b89903177c59f633bcd96d29eaa7793
Peter Deme <snypox@gmail.com>
2024-01-11 12:49:44 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,16 @@
+resource "random_pet" "pet1" {
+  length = 7
+}
+
 resource "random_pet" "pet2" {
   length = 7
 }
 
+resource "random_pet" "pet3" {
+  length = 7
+}
 
 output "output" {
-  value     = random_pet.pet2.id
+  value     = random_pet.pet1.id
   sensitive = false
 }
